6e3f6d772033366688143e815ad9b3795754805c
syang <syang.pstar@outlook.com>
2023-04-21 21:21:33 +0800

    sync

--- codeforces/doing/1777-C.cpp
+++ codeforces/doing/1777-C.cpp
@@ -2,6 +2,10 @@
 #define ll long long int
 #define GETA(buf,size) for(int i=0;i<size;i++) cin>>buf[i];
 #define rep(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define FASTIO                \
+    ios ::sync_with_stdio(0); \
+    cin.tie(0);               \
+    cout.tie(0);
 using namespace std;
 const int N = 1e5 + 5;
 const int M = 1e5 + 5;
@@ -11,7 +15,7 @@ void init(){
     int maxn = N;
     int maxm = M;
     rep(i,1,maxn){
-        for(int j = 1;j<maxm;j+=i){
+        for (int j = 1; j < maxm; j += i){
             factors[j].push_back(i);
         }
     }
@@ -26,11 +30,8 @@ void solve(){
 }
 int main()
 {
-#ifndef ONLINE_JUDGE
-    freopen("/root/devel/algorithm/in.in","r",stdin); 
-#endif
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    FASTIO;
+    init();
     int t;cin>>t;
     while(t--) solve();
     return 0;
